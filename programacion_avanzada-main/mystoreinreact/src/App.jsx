f5a7b1efddb28e36445f46854b91ae2872122f0e
209 <209@academicos.cesde.edu.co>
2021-09-15 21:15:52 -0500

    Update App.jsx
    Adding a Grid

--- programacion_avanzada-main/mystoreinreact/src/App.jsx
+++ programacion_avanzada-main/mystoreinreact/src/App.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { Button } from '@material-ui/core';
+import { Button, Grid } from '@material-ui/core';
 
 function App() {
   return (
     <div>
+      <Grid container>
+        <Grid item xs={6}>
         <Button variant="contained" color='secondary'>ENTER</Button>
+        </ Grid>
+        <Grid item xs={6}>
+        <Button variant="contained" color='primary'>EXIT</Button>
+        </ Grid>
+      </Grid>
     </div>
   );
 }
